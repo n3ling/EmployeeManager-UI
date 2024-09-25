163b21cbfccfcf5dffc44ffc21f904e16d5a72be
Jimmy Tran <jtran110@myseneca.ca>
2024-09-25 18:43:58 -0400

    Completed all CRUD, will consider validation after

--- components/ShiftScheduler.tsx
+++ components/ShiftScheduler.tsx
@@ -1,22 +1,9 @@
 import { useState, useEffect } from 'react';
 import { ScheduleEventType, WeeklySchedule } from 'react-beautiful-schedule';
-
-// const eventsData = [
-//   {
-//     id: '1',
-//     title: 'Event 1',
-//     start: '2023-09-24T09:00:00',
-//     end: '2023-09-24T10:15:00',
-//   },
-//   {
-//     id: '2',
-//     title: 'Event 2',
-//     start: '2024-09-23T10:00:00',
-//     end: '2024-09-23T11:30:00',
-//     description: 'This is a description',
-//     defaultTheme: 'yellow' as const,
-//   },
-// ];
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { Form } from 'react-bootstrap';
+import { addShift, updateShift, deleteShift } from '@/lib/shiftData';
 
 function ShiftScheduler() {
   const [data, setData] = useState([]);
@@ -24,22 +11,38 @@ function ShiftScheduler() {
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [key, setKey] = useState(0)
   const [selectedDate, setSelectedDate] = useState(new Date())
-  const [clickedEvent, setClickedEvent] = useState<ScheduleEventProps | null>(null)
-  const [tempEvent, setTempEvent] = useState<ScheduleEventProps | null>(null)
 
-  useEffect(() => {
-    fetch('https://employeemanager-y5z4.onrender.com/shift')
-      .then(res => res.json())
-      .then(data => setData(data))
-      .catch(err => console.error('Error fetching data:', err));
-  }, []);
+  const [shift, setShift] = useState({
+    shiftID: 0, 
+    shiftDate: '',
+    startTime: '',
+    endTime: '',
+    isHoliday: false
+  });
+
+  const formatDate = (date) => {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = (`0${d.getMonth() + 1}`).slice(-2); // Add leading 0 for single digits
+    const day = (`0${d.getDate()}`).slice(-2); // Add leading 0 for single digits
+    return `${year}-${month}-${day}`;
+  };
+
+  const handleInputChange = (e) => {
+    const { id, value, type, checked } = e.target;
+    setShift((prevShift) => ({
+      ...prevShift,
+      [id]: type === 'checkbox' ? checked : value,
+    }));
+  };
 
   const transformDataToEvents = (data) => {
     return {
       id: data.shiftID.toString(),
       title: `Shift ${data.shiftID}`,
       start: `${data.shiftDate}T${data.startTime}`,
-      end: `${data.shiftDate}T${data.endTime}`
+      end: `${data.shiftDate}T${data.endTime}`,
+      defaultTheme: data.isHoliday > 0 ? 'yellow' : 'blue'
     }
   }
 
@@ -47,14 +50,31 @@ function ShiftScheduler() {
     return data.map(shift => transformDataToEvents(shift));
   }
 
-  useEffect(() => {
-    const temp = transformAllData(data);
-    setEvents(temp);
-  }, []);
+  const findHighestID = (data) => {
+    if (data.length === 0) return 0; // Handle empty data
 
+    return data.reduce((maxID, shift) => {
+      return shift.shiftID > maxID ? shift.shiftID : maxID;
+    }, 0);
+  }
+
+  useEffect(() => {
+    fetch('https://employeemanager-y5z4.onrender.com/shift')
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => console.error('Error fetching data:', err));
+  }, []);
+  
+  useEffect(() => {
+    if (data.length > 0) {
+      const temp = transformAllData(data);
+      setEvents(temp);
+    }
+  }, [data]); 
+
+  console.log(events)
 
   function showModal() {
-    setKey((prev) => prev + 1)
     setIsModalVisible(true)
   }
 
@@ -63,28 +83,129 @@ function ShiftScheduler() {
   }
 
   function handleAddEvent(date: Date) {
-    setSelectedDate(date)
-    setClickedEvent(null)
-    setTempEvent(null)
+    setKey(null)
+    setShift({
+      shiftDate: formatDate(date),
+      startTime: '',
+      endTime: '',
+      isHoliday: false
+    })
     showModal()
   }
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(shift);
+
+    try {
+        let success;
+
+        if (key) { 
+            success = await updateShift(shift);
+            if (success) {
+                console.log("Shift updated successfully");
+            } else {
+                console.error("Failed to update shift");
+            }
+        } else {
+            success = await addShift(shift); 
+            if (success) {
+                console.log("Shift added successfully");
+            } else {
+                console.error("Failed to add shift");
+            }
+        }
+
+        if (success) {
+            window.location.reload(); 
+        }
+
+    } catch (error) {
+        console.error("An error occurred while processing the shift:", error);
+    }
+}
+
+
   function handleClickEvent(day: Date, dayISO: string | null, event: ScheduleEventProps) {
-    console.log({ day, dayISO, event })
-    setSelectedDate(day)
-    setClickedEvent(event)
+    setKey(event.id)
+    console.log(event.id)
+    setShift({
+      shiftID: event.id, 
+      shiftDate: formatDate(day),
+      startTime: event.start.split('T')[1],
+      endTime: event.end.split('T')[1],
+      isHoliday: event.defaultTheme == "blue"? false : true
+    })
     showModal()
   }
+
+  async function handleDelete(e, id){
+    e.stopPropagation();
+    try{
+      await deleteShift(id);
+      window.location.reload();
+    } catch(err){
+      console.error('Error deleting employee:', err);
+    }
+  };
 
   return (
     <>
-      <WeeklySchedule
-        style={{ padding: '24px' }}
-        showDateNavigator
-        events={events}
-        onAddEvent={handleAddEvent}
-        onClickEvent={handleClickEvent}
-      />
+
+      <Modal show={isModalVisible} onHide={hideModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{key == null ? 'Add Shift' : 'Edit Shift'}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="shiftDate">
+              <Form.Label>Shift Date</Form.Label>
+              <Form.Control 
+                type="date"  
+                value={shift.shiftDate}
+                readOnly
+                required 
+              />
+            </Form.Group>
+            <Form.Group controlId="startTime">
+              <Form.Label>Start Time</Form.Label>
+              <Form.Control 
+                type="time" 
+                value={shift.startTime}
+                onChange={handleInputChange} 
+                required 
+              />
+            </Form.Group>
+            <Form.Group controlId="endTime">
+              <Form.Label>End Time</Form.Label>
+              <Form.Control 
+                type="time" 
+                value={shift.endTime}
+                onChange={handleInputChange} 
+                required 
+              />
+            </Form.Group>
+            <Form.Group controlId="isHoliday">
+              <Form.Check 
+                type="checkbox" 
+                label="Is Holiday?" 
+                checked={shift.isHoliday}
+                onChange={handleInputChange} 
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={(e) => handleDelete(e, key)}>
+            Delete
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <WeeklySchedule style={{ padding: '24px' }} showDateNavigator events={events} onAddEvent={handleAddEvent} onClickEvent={handleClickEvent}/>
     </>
 
   );
